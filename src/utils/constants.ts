fac9f07848371fe59a1d533c0ec17722c91c1c60
ppincore <liltracyn@ya.ru>
2025-02-02 19:58:08 +0300

    add: space objects data

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,21 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+import { IBaseSpaceObject, ISatellite, IPlanet } from "../types";
+const moon: ISatellite = {
+  name: "Moon",
+  mass: 7.348e22,
+  type: "satellite",
+  distanceFromPlanet: 3.844e8,
+  orbitalSpeed: 1022,
+  rotationSpeed: 2.6617e-6,
+};
 
+const earth: IPlanet = {
+  name: "Earth",
+  mass: 5.972e24,
+  type: "planet",
+  distanceFromStar: 149.6e6,
+  orbitalSpeed: 29.78,
+  rotationSpeed: 7.2921159e-5,
+  satellites: [moon],
+};
+
+export const spaceObjects: IBaseSpaceObject[] = [moon, earth];
