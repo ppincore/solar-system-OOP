9417333c713a13a4489f663a7bd366eafd1ab8b1
ppincore <liltracyn@ya.ru>
2025-02-02 19:57:51 +0300

    add: types for planet and satellite

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,18 @@
-export interface IStar {
-	name:string
-	mass:number
-}
+export interface IBaseSpaceObject {
+	name: string;
+	mass: number;
+	type: 'planet' | 'satellite'
+	orbitalSpeed: number;
+	rotationSpeed: number;
+  }
 
-export interface IPlanet {
-	name:string
-	mass:number
-	distanceFromStar:number
-	orbitalSpeed:number
-	rotationSpeed:number
-	satellites:ISatellite[]
-	calculateOrbitalPeriod(): number 
-	calculateDayLength():number
-	addSatellite(items:ISatellite):void
-}
+  export interface ISatellite extends IBaseSpaceObject{
+	type: 'satellite',
+	distanceFromPlanet: number,
 
-export type IPlanetConfig = Omit<IPlanet,'calculateOrbitalPeriod' | 'calculateDayLength' | 'addSatellite'>
-export type ISatelliteConfig = Omit<ISatellite,'calculateOrbitalPeriod' | 'calculateDayLength'>
-
-export interface ISatellite{
-	name:string
-	mass:number
-	distanceFromPlanet:number
-	orbitalSpeed:number
-	rotationSpeed:number
-	calculateOrbitalPeriod(): number 
-	calculateDayLength():number
-}
+  }
+  export interface IPlanet extends IBaseSpaceObject {
+	type: 'planet';
+	distanceFromStar: number;
+	satellites: ISatellite[];
+  }
